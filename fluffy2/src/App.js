bde784a215526998b95cfe9c7195abbb88c8b407
Andrei Kirkouski <andrew@spiralscout.com>
2025-01-01 15:59:43 +0100

    react on a b keyboard

--- fluffy2/src/App.js
+++ fluffy2/src/App.js
@@ -7,18 +7,18 @@ import * as Tone from "tone";
 
 const AGENT_ID = 'pvzY8rKHqL2Aror6te57';
 
-
 const vol = new Tone.Volume(-12).toDestination();
 const synth = new Tone.Synth().connect(vol).toDestination();
 synth.volume.value = -12;
 
 function App() {
   const [inCall, setInCall] = useState(false);
+  const [playTone, setPlayTone] = useState(false);
+  const [buttonActive, setButtonActive] = useState(false);
   const conversation = useConversation({
     onConnect: (p) => {
       console.log('connected', p)
-      const now = Tone.now();
-      synth.triggerRelease(now)
+      setPlayTone(false);
       setInCall(true)
     },
     onMessage: (p) => console.log('message', p),
@@ -27,28 +27,76 @@ function App() {
     onError: (p) => console.log('error', p),
   });
   const conversationId = useRef('');
-  const startCall = async ()=>{
-    // request user michrophone
-    await navigator.permissions.query({ name: 'microphone' })
-    await navigator.mediaDevices.getUserMedia({ audio: true, video: false });
-    conversationId.current = await conversation.startSession({ agentId: AGENT_ID });
+
+  useEffect(()=>{
+    if(playTone) {
+        const now = Tone.now();
+        synth.triggerAttack(400, now)
+    } else {
+        const now = Tone.now();
+        synth.triggerRelease(now)
+    }
+  }, [playTone]);
+
+  useEffect(()=>{
+    if(buttonActive) {
+      const startCall = async ()=>{
+        // request user michrophone
+        await navigator.permissions.query({ name: 'microphone' })
+        await navigator.mediaDevices.getUserMedia({ audio: true, video: false });
+        conversationId.current = await conversation.startSession({ agentId: AGENT_ID });
+      }
+      startCall();
+    } else {
+      conversation.endSession();
+      setPlayTone(true);
+      setInCall(false);
+    }
+  }, [buttonActive]);
+
+  const throttleTimer = useRef(null);
+
+  const throttleKeyPressed = () => {
+    if (throttleTimer.current) {
+        clearTimeout(throttleTimer.current);
+    }
+    setButtonActive(true);
   }
 
-  const stopCall = async () => {
-    await conversation.endSession();
-    const now = Tone.now();
-    synth.triggerAttack(400, now)
-    setInCall(false)
+  const throttleKeyReleased = () => {
+    if (throttleTimer.current) {
+        clearTimeout(throttleTimer.current);
+    }
+    throttleTimer.current = setTimeout(() => {
+        setButtonActive(false);
+    }, 500);
   }
+
+  useEffect(()=>{
+    window.addEventListener("keydown", (event) => {
+      console.log('keydown', event.key);
+        if (event.key === 'a' || event.key==='b') {
+            throttleKeyPressed();
+        }
+    });
+    window.addEventListener("keyup", (event) => {
+      console.log('keyup', event.key);
+        if (event.key === 'a' || event.key==='b') {
+            throttleKeyReleased();
+        }
+    });
+  });
 
   return (
     <div className="App"
-         onPointerDown={startCall}
-         onPointerUp={stopCall}
+
     >
       <header className="App-header">
         <img src={inCall?boykisser:boykisser2} className="App-logo" alt="logo" />
-        <p>Tap and hold to Talk</p>
+        <p>Press and hold 'a' or 'b' button to talk</p>
+        <button onPointerDown={throttleKeyPressed}
+                onPointerUp={throttleKeyReleased}>This one works too</button>
+        <p>{buttonActive?'Phone is ON':'Phone is OFF'}</p>
       </header>
     </div>
   );
